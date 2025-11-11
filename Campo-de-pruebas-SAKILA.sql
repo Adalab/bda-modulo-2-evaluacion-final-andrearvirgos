2d541232961bf35eaa77ba67595bd4b13eb88427
Andrea R.Virgos <andrearvirgos@gmail.com>
2025-11-11 20:19:00 +0100

    ejercicio evaluación completado

--- Campo-de-pruebas-SAKILA.sql
+++ Campo-de-pruebas-SAKILA.sql
@@ -45,7 +45,6 @@ SELECT *
 	FROM actor; 
     
 -- 6: 
-
 SELECT first_name, last_name
 	FROM actor
     WHERE last_name LIKE '%Gibson%'; -- por si hubiera algún apellido compuesto, que no 
@@ -153,3 +152,200 @@ SELECT a.actor_id, a.first_name, a.last_name
 	WHERE fa.film_id IS NULL; 
 
 -- 16:
+SELECT *
+	FROM film;
+
+SELECT title
+	FROM film 
+	WHERE release_year BETWEEN 2005 AND 2010; 
+
+SELECT title, release_year -- un par de comprobaciones, porque devuelve las mil filas
+	FROM film
+    ORDER BY release_year DESC; 
+
+SELECT release_year, COUNT(title) AS total_pelis -- ok, todas las pelis indexadas en la BD son del año de lanzamiento 2006!
+	FROM film
+    WHERE release_year BETWEEN 2005 AND 2010
+    GROUP BY release_year; 
+
+-- 17:
+SELECT *
+	FROM category; -- comprobación "family" es un género/categoría Y TIENE ID 8
+
+SELECT title 
+	FROM film AS f
+    INNER JOIN film_category AS fic
+		ON f.film_id = fic.film_id
+        WHERE fic.category_id IN (
+			SELECT category_id
+				FROM category
+                WHERE name = 'Family'
+); 
+
+SELECT f.title, fic.category_id -- si, todos los títulos tienen el id 8 de Family.
+	FROM film AS f
+    INNER JOIN film_category AS fic
+		ON f.film_id = fic.film_id
+        WHERE fic.category_id IN (
+			SELECT category_id
+				FROM category
+                WHERE name = 'Family'
+); 
+
+-- 18:
+SELECT a.first_name, a.last_name, COUNT(fa.film_id) 
+	FROM actor AS a
+	INNER JOIN film_actor AS fa
+		ON a.actor_id = fa.actor_id
+        GROUP BY a.actor_id 
+			HAVING COUNT(fa.film_id) > 10; 
+            
+SELECT a.first_name, a.last_name, COUNT(fa.film_id) AS total_films
+	FROM actor AS a
+	INNER JOIN film_actor AS fa
+		ON a.actor_id = fa.actor_id
+        GROUP BY a.actor_id 
+			HAVING total_films > 10; 
+            
+-- 19:
+SELECT title, rating, length
+	FROM film
+    WHERE rating = 'R' AND length > 120; 
+    
+SELECT title -- select limpio porque solo piden título de las pelis
+	FROM film
+    WHERE rating = 'R' AND length > 120; 
+
+SELECT COUNT(*) AS pelis_cumplen_condiciones
+	FROM film
+    WHERE rating = 'R' AND length > 120; -- query de comprobación, resultado 90 filas/pelis ok
+
+    
+-- 20:
+-- categoría peli = name en category
+-- AVG length > 120
+-- SELECT name + AVG length
+SELECT *
+	FROM category; -- + consulta diagrama para visualizar cómo están conectadas las tablas: category (category_id)-> film_category(film_id)-> film
+
+SELECT ca.name, AVG(length)
+	FROM category AS ca
+    INNER JOIN film_category AS fic
+		ON ca.category_id = fic.category_id
+	INNER JOIN film AS f
+		ON fic.film_id = f.film_id
+	GROUP BY ca.name
+		HAVING AVG(length) > 120; 
+
+SELECT ca.name AS name_genre, AVG(length) AS average_duration -- devuelve resultado más comprensible
+	FROM category AS ca
+    INNER JOIN film_category AS fic
+		ON ca.category_id = fic.category_id
+	INNER JOIN film AS f
+		ON fic.film_id = f.film_id
+	GROUP BY name_genre
+		HAVING average_duration > 120;
+        
+
+SELECT ca.name AS name_genre, AVG(length) AS average_duration -- query anterior devuelve 4filas(Drama, Foreign, Games, Sports)
+	FROM category AS ca
+    INNER JOIN film_category AS fic
+		ON ca.category_id = fic.category_id
+	INNER JOIN film AS f
+		ON fic.film_id = f.film_id
+	GROUP BY name_genre
+		ORDER BY  average_duration DESC; -- cambiando el having por un order by en descendente, compruebo que las 4primeras filas, son las que superan los 120 y las mismas que devuelve mi query final.
+        
+-- 21:
+-- actor(actor_id) -> film_actor(film_id) -> film >= 5 pelis
+-- select first_name + nº pelis
+
+SELECT a.first_name, COUNT(fa.film_id) AS total_films   
+	FROM actor AS a
+    INNER JOIN film_actor AS fa
+		ON a.actor_id = fa.actor_id
+        GROUP BY a.actor_id, a.first_name
+			HAVING total_films >= 5; 
+
+SELECT a.first_name, COUNT(fa.film_id) AS total_films   -- query de comprobación
+	FROM actor AS a
+    INNER JOIN film_actor AS fa
+		ON a.actor_id = fa.actor_id
+        GROUP BY a.actor_id, a.first_name
+			ORDER BY total_films ASC; -- No hay actores con menos de 14pelis. 14 > 5 con lo que el HAVING total_films >= 5 es correcto, los 200 actores cumplen la condición.
+
+-- 22: 
+SELECT title, rental_duration -- para mi, la query más eficiente: concisa, limpia y devuelve lo que se busca. Pero no cumple estrictamente con el enunciado/subconsulta.
+	FROM film 
+    WHERE rental_duration > 5; 
+
+-- no entiendo el requisito de la subconsulta aquí, es redundante porque filtra todo el rato la misma tabla. Me parece complicarlo! PERO, si es requisito del enunciado, por seguir practicvando sintaxis de subconsultas:
+SELECT title, rental_duration 
+	FROM film 
+    WHERE film_id IN (  
+		SELECT film_id 
+			FROM film
+            WHERE  rental_duration > 5
+);
+
+
+-- 23:
+SELECT *
+	FROM category; -- category_id 'horror' es 11
+
+-- actores IN y actores NOT IN 'horror'
+SELECT a.first_name, a.last_name
+	FROM actor AS a
+		WHERE a.actor_id NOT IN (
+			SELECT fa.actor_id
+				FROM film_actor AS fa
+                INNER JOIN film_category AS fic
+					ON fa.film_id = fic.film_id
+				INNER JOIN category AS ca
+					ON fic.category_id = ca.category_id
+				WHERE ca.name = 'Horror'
+	
+);
+
+-- query de comprobación:
+SELECT COUNT(DISTINCT fa.actor_id)  -- 156 actores distintos que si han actuado en pelis horror(que es la lista de exclusión que genera el NOT IN y por la que esa query devuelve solo 44 actores!!)
+	FROM film_actor AS fa
+    INNER JOIN film_category AS fic
+		ON fa.film_id = fic.film_id
+	INNER JOIN category AS ca
+		ON fic.category_id = ca.category_id
+        WHERE ca.name = 'Horror'; 
+        
+SELECT *
+	FROM actor;  -- hay 200 actores registrados en la BD(200-156= 44 actores que devuelve la query final/oficial)
+
+
+-- 24:
+-- film-> film_category-> category
+
+-- siendo estrictos con el enunciado, la consulta devolvería 0, porque no hay 'comedias' entre las categorías.
+SELECT f.title
+	FROM film AS f
+    INNER JOIN film_category AS fic
+		ON f.film_id = fic.film_id
+	INNER JOIN category AS ca
+		ON fic.category_id = ca.category_id
+	WHERE ca.name = 'Comedia' AND f.length > 180; 
+
+-- interpretando que buscamos 'comedy' (factible al conocer la traducción, si la BD estuviera en un idioma de complicada traducción, la extracción de datos se complicaría no teniendo las pautas correctas de consulta)
+SELECT f.title
+	FROM film AS f
+    INNER JOIN film_category AS fic
+		ON f.film_id = fic.film_id
+	INNER JOIN category AS ca
+		ON fic.category_id = ca.category_id
+	WHERE ca.name = 'Comedy' AND f.length > 180; 
+
+-- query de comprobación, mostrando columnas:
+SELECT f.title, f.length AS minutaje, ca.name AS categoria_control
+	FROM film AS f
+    INNER JOIN film_category AS fic
+		ON f.film_id = fic.film_id
+	INNER JOIN category AS ca
+		ON fic.category_id = ca.category_id
+	WHERE ca.name = 'Comedy' AND f.length > 180;
--- Ejercicio-evaluacion-final.sql
+++ Ejercicio-evaluacion-final.sql
@@ -9,48 +9,48 @@ SELECT title
 	FROM film
     WHERE rating = 'PG-13';
     
--- 3.  Encuentra el título y la descripción de todas las películas que contengan la palabra "amazing" en su descripción: 
+-- 3. Encuentra el título y la descripción de todas las películas que contengan la palabra "amazing" en su descripción: 
 SELECT title, description
 	FROM film
     WHERE  description LIKE '%amazing%'; 
     
--- 4.  Encuentra el título de todas las películas que tengan una duración mayor a 120 minutos:
+-- 4. Encuentra el título de todas las películas que tengan una duración mayor a 120 minutos:
 SELECT title
 	FROM film
     WHERE length > 120; 
 
--- 5.  Recupera los nombres de todos los actores:
+-- 5. Recupera los nombres de todos los actores:
 SELECT first_name
 	FROM actor; 
 
--- 6.  Encuentra el nombre y apellido de los actores que tengan "Gibson" en su apellido:
+-- 6. Encuentra el nombre y apellido de los actores que tengan "Gibson" en su apellido:
 SELECT first_name, last_name
 	FROM actor
     WHERE last_name = 'Gibson'; 
 
--- 7.  Encuentra los nombres de los actores que tengan un actor_id entre 10 y 20:
+-- 7. Encuentra los nombres de los actores que tengan un actor_id entre 10 y 20:
 SELECT first_name
 	FROM actor
 	WHERE actor_id >= 10 AND actor_id <= 20;
 
--- 8.  Encuentra el título de las películas en la tabla film que no sean ni "R" ni "PG-13" en cuanto a su clasificación:
+-- 8. Encuentra el título de las películas en la tabla film que no sean ni "R" ni "PG-13" en cuanto a su clasificación:
 SELECT title
 	FROM film
     WHERE rating NOT IN ('PG-13', 'R'); 
     
--- 9:  Encuentra la cantidad total de películas en cada clasificación de la tabla film y muestra la clasificación junto con el recuento:
+-- 9. Encuentra la cantidad total de películas en cada clasificación de la tabla film y muestra la clasificación junto con el recuento:
 SELECT rating, COUNT(title)
 	FROM film
     GROUP BY rating; 
 
--- 10.  Encuentra la cantidad total de películas alquiladas por cada cliente y muestra el ID del cliente, su nombre y apellido junto con la cantidad de películas alquiladas:
+-- 10. Encuentra la cantidad total de películas alquiladas por cada cliente y muestra el ID del cliente, su nombre y apellido junto con la cantidad de películas alquiladas:
 SELECT c.customer_id, c.first_name, c.last_name, COUNT(r.rental_id) AS rented_films
 	FROM customer AS c
     INNER JOIN rental AS r
 		ON c.customer_id = r.customer_id
 	GROUP BY customer_id, c.first_name, c.last_name;  
     
--- 11.  Encuentra la cantidad total de películas alquiladas por categoría y muestra el nombre de la categoría junto con el recuento de alquileres:
+-- 11. Encuentra la cantidad total de películas alquiladas por categoría y muestra el nombre de la categoría junto con el recuento de alquileres:
 SELECT ca.name AS name_genre,  COUNT(r.rental_id) AS rented_films
 	FROM category AS ca
     INNER JOIN film_category AS fic
@@ -66,7 +66,7 @@ SELECT rating, AVG(length)
 	FROM film
     GROUP BY rating; 
     
--- 13.  Encuentra el nombre y apellido de los actores que aparecen en la película con title "Indian Love":
+-- 13. Encuentra el nombre y apellido de los actores que aparecen en la película con title "Indian Love":
 SELECT a.first_name, a.last_name
 	FROM actor AS a
     INNER JOIN film_actor AS fa
@@ -75,7 +75,7 @@ SELECT a.first_name, a.last_name
 		ON fa.film_id = f.film_id
     WHERE f.title = 'INDIAN LOVE'; 
 
--- 14.  Muestra el título de todas las películas que contengan la palabra "dog" o "cat" en su descripción.
+-- 14. Muestra el título de todas las películas que contengan la palabra "dog" o "cat" en su descripción.
 SELECT title
 	FROM film
     WHERE description LIKE '%dog%' OR description LIKE '%cat%'; 
@@ -87,9 +87,91 @@ SELECT a.actor_id, a.first_name, a.last_name
 		ON a.actor_id = fa.actor_id
 	WHERE fa.film_id IS NULL; 
     
--- 16.  Encuentra el título de todas las películas que fueron lanzadas entre el año 2005 y 2010:
+-- 16. Encuentra el título de todas las películas que fueron lanzadas entre el año 2005 y 2010:
+SELECT title
+	FROM film 
+	WHERE release_year BETWEEN 2005 AND 2010; 
+
+-- 17. Encuentra el título de todas las películas que son de la misma categoría que "Family":
+SELECT title 
+	FROM film AS f
+    INNER JOIN film_category AS fic
+		ON f.film_id = fic.film_id
+        WHERE fic.category_id IN (
+			SELECT category_id
+				FROM category
+                WHERE name = 'Family'
+); 
  
+-- 18. Muestra el nombre y apellido de los actores que aparecen en más de 10 películas:
+SELECT a.first_name, a.last_name, COUNT(fa.film_id) AS total_films
+	FROM actor AS a
+	INNER JOIN film_actor AS fa
+		ON a.actor_id = fa.actor_id
+        GROUP BY a.actor_id 
+			HAVING total_films > 10; 
+            
+-- 19. Encuentra el título de todas las películas que son "R" y tienen una duración mayor a 2 horas en la tabla film:
+SELECT title
+	FROM film
+    WHERE rating = 'R' AND length > 120; 
+ 
+-- 20. Encuentra las categorías de películas que tienen un promedio de duración superior a 120 minutos y muestra el nombre de la categoría junto con el promedio de duración:
+SELECT ca.name AS name_genre, AVG(length) AS average_duration 
+	FROM category AS ca
+    INNER JOIN film_category AS fic
+		ON ca.category_id = fic.category_id
+	INNER JOIN film AS f
+		ON fic.film_id = f.film_id
+	GROUP BY name_genre
+		HAVING average_duration > 120; 
+
+-- 21. Encuentra los actores que han actuado en al menos 5 películas y muestra el nombre del actor junto con la cantidad de películas en las que han actuado:
+SELECT a.first_name, COUNT(fa.film_id) AS total_films   -- Solo el nombre, por estricto cumplimiento del enunciado(en otros se explicita nombre+apellido, así no añado datos innecesarios que ensucien el resultado).
+	FROM actor AS a
+    INNER JOIN film_actor AS fa
+		ON a.actor_id = fa.actor_id
+        GROUP BY a.actor_id, a.first_name
+			HAVING total_films >= 5; 
+
+-- 22. Encuentra el título de todas las películas que fueron alquiladas por más de 5 días. Utiliza una subconsulta para encontrar los rental_ids con una duración superior a 5 días y luego selecciona las películas correspondientes
+SELECT title, rental_duration 
+	FROM film 
+    WHERE film_id IN (  
+		SELECT film_id 
+			FROM film
+            WHERE  rental_duration > 5
+);
+
+-- versión más eficiente en la práctica, pero no alineada con la orden del enunciado:
+SELECT title, rental_duration 
+	FROM film 
+    WHERE rental_duration > 5; 
+    
+
+-- 23. Encuentra el nombre y apellido de los actores que no han actuado en ninguna película de la categoría "Horror". Utiliza una subconsulta para encontrar los actores que han actuado en películas de la categoría "Horror" y luego exclúyelos de la lista de actores.
+SELECT a.first_name, a.last_name  -- devuelve 44 actores que no han participado en pelis horror
+	FROM actor AS a
+		WHERE a.actor_id NOT IN (
+			SELECT fa.actor_id
+				FROM film_actor AS fa
+                INNER JOIN film_category AS fic
+					ON fa.film_id = fic.film_id
+				INNER JOIN category AS ca
+					ON fic.category_id = ca.category_id
+				WHERE ca.name = 'Horror'
+	
+);
 
 
- 
+-- 24. Encuentra el título de las películas que son comedias y tienen una duración mayor a 180 minutos en la tabla film:
+-- (Interpretando y traduciendo 'comedias' a 'comedy' que es como está indexada esa categoría)
+SELECT f.title
+	FROM film AS f
+    INNER JOIN film_category AS fic
+		ON f.film_id = fic.film_id
+	INNER JOIN category AS ca
+		ON fic.category_id = ca.category_id
+	WHERE ca.name = 'Comedy' AND f.length > 180; 
+  
 
